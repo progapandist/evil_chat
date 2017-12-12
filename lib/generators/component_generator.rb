7b20bb09ebefd7cf4c9953e45e09228dd1cfc959
Andy B <andybarnov@gmail.com>
2017-12-12 09:31:09 +0100

    improve generator code

--- lib/generators/component_generator.rb
+++ lib/generators/component_generator.rb
@@ -2,15 +2,16 @@ class ComponentGenerator < Rails::Generators::Base
   argument :component_name, required: true, desc: "Component name, e.g: button"
 
   def create_view_file
-    create_file "#{component_path}" + "_#{component_name}.html.erb"
+    create_file "#{component_path}/_#{component_name}.html.erb"
   end
 
   def create_css_file
-    create_file "#{component_path}" + "#{component_name}.css"
+    create_file "#{component_path}/#{component_name}.css"
   end
 
   def create_js_file
-    create_file "#{component_path}" + "#{component_name}.js" do
+    create_file "#{component_path}/#{component_name}.js" do
+      # require component's CSS inside JS automatically
       "import \"./#{component_name}.css\";\n"
     end
   end
@@ -18,6 +19,6 @@ class ComponentGenerator < Rails::Generators::Base
   protected
 
   def component_path
-   "frontend/components/#{component_name}/"
+   "front/components/#{component_name}"
   end
 end
