4c7f90f7e2fc4c5aa44f1f69884b2d167e3a000c
Andy B <andybarnov@gmail.com>
2017-12-17 17:03:45 +0100

    SECRET_KEY_BASE for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
-
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
